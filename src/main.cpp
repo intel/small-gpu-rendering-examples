35ee412bb2383f26a71d65d91cb41f6dff517520
Martina Kollarova <mkollaro@gmail.com>
2015-04-16 10:58:15 +0300

    compile vshader from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,61 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <stdexcept>
 #include <GL/glew.h>
 #include <SDL.h>
 #include <SDL_opengl.h>
+
+#ifndef SHADERS_DIR
+#define SHADERS_DIR "./shaders/"
+#endif
+
+const float vertices[] = {
+     0.0f,  0.5f,
+     0.5f, -0.5f,
+    -0.5f, -0.5f
+};
 
 void keyPressHandler(const SDL_Event& event) {
     if (event.type != SDL_KEYDOWN) return;
 
 }
 
+const std::string readFile(const std::string& filepath) {
+    std::ifstream f(filepath);
+    if (!f.is_open()) {
+        std::cerr << "Unable to open file " << filepath << std::endl;
+        throw std::runtime_error("I/O Error");
+    }
+    std::string str((std::istreambuf_iterator<char>(f)),
+                     std::istreambuf_iterator<char>());
+    return str;
+}
 
-int main(int argc, char *argv[])
-{
+enum class ShaderType {vertex, fragment};
+
+void compileShader(const std::string& filename, ShaderType type) {
+    auto source = readFile(SHADERS_DIR + filename).c_str();
+    auto gl_type = GL_VERTEX_SHADER;
+    if (type != ShaderType::vertex) gl_type = GL_FRAGMENT_SHADER;
+
+    GLuint shader = glCreateShader(gl_type);
+    glShaderSource(shader, 1, &source, NULL);
+    glCompileShader(shader);
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status != GL_TRUE) {
+        std::cerr << "Failed to compile shader:\n";
+        char buffer[512];
+        std::cerr << source << std::endl;
+        std::cerr << "************************************\n";
+        glGetShaderInfoLog(shader, 512, NULL, buffer);
+        std::cerr << buffer;
+    }
+}
+
+int main(int argc, char *argv[]) {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
                         SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
@@ -21,6 +67,13 @@ int main(int argc, char *argv[])
     SDL_GLContext context = SDL_GL_CreateContext(window);
     glewExperimental = GL_TRUE;
     glewInit();
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+                 GL_STATIC_DRAW);
+    compileShader("vshader.glsl", ShaderType::vertex);
 
     SDL_Event event;
     bool quit = false;
