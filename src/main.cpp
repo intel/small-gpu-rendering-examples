3e73625e84f05737f476658b4ee5758444bc3ab0
Martina Kollarova <mkollaro@gmail.com>
2015-04-16 11:24:11 +0300

    more refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -74,16 +74,21 @@ GLuint initShaders() {
     return shaderProgram;
 }
 
-int main(int argc, char *argv[]) {
+SDL_GLContext initContext(SDL_Window *window) {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
                         SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    return context;
+}
+
+int main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("Hello World",
                                            100, 100, 800, 600,
                                            SDL_WINDOW_OPENGL);
-    SDL_GLContext context = SDL_GL_CreateContext(window);
+    auto context = initContext(window);
     glewExperimental = GL_TRUE;
     glewInit();
 
